95793fc06392c391dbc91a2e332b60bb52fd97d4
Stefan Machmeier <stefan-machmeier@outlook.com>
2020-07-18 11:42:43 +0200

    Update to tar instead of images

--- image_loader.py
+++ image_loader.py
@@ -1,12 +1,14 @@
-import torch
-import torchvision
+import os
+import shutil
 import torchxrayvision as xrv
-from tqdm import tqdm
-import sys
+import tarfile
 
+FILE_NAME = './data/covid-19-dataset.tar.gz'
 
 def getDataset(imgpath='./data/images', csvpath='./data/metadata.csv'):
     print('[INFO] Get dataset')
+    if not os.path.exists(imgpath):
+        shutil.unpack_archive(FILE_NAME, imgpath)
     d_covid19 = xrv.datasets.COVID19_Dataset(views=["PA", "AP", "AP Supine"],
                                              imgpath=imgpath,
                                              csvpath=csvpath)
