788d4d82f7c38dfa9e5f2a9334b57ab405a45c0f
Stefan Machmeier <stefan-machmeier@outlook.com>
2020-08-07 10:49:17 +0200

    fix roc plots and add mean for acc and loss

--- binary_classifier/binary_network_pytorch.py
+++ binary_classifier/binary_network_pytorch.py
@@ -2,7 +2,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.utils.data as data_utils
-from sklearn import metrics
 from sklearn.model_selection import train_test_split
 
 from binary_classifier.model import Net
@@ -12,8 +11,8 @@ from utils import plot_loss, plot_acc, plot_roc_binary
 
 def train_model(images, labels, path, epochs=10, learning_rate=0.0001, batch_size=32):
     net = Net()
-    train_loss, test_loss = [], []
-    train_acc, test_acc = [], []
+    train_loss, test_loss,  = [], []
+    train_acc, test_acc,  = [], []
     roc_score = []
     roc_true = []
 
@@ -30,6 +29,9 @@ def train_model(images, labels, path, epochs=10, learning_rate=0.0001, batch_siz
     test_loader = torch.utils.data.DataLoader(test_data, shuffle=True, batch_size=batch_size)
 
     for epoch in range(epochs):
+        train_loss_it, train_acc_it = [], []
+        test_loss_it, test_acc_it = [], []
+        roc_score_it, roc_true_it = [], []
         net.train()
         total_step = len(train_loader)
         for i, (images, labels) in enumerate(train_loader):
@@ -55,9 +57,11 @@ def train_model(images, labels, path, epochs=10, learning_rate=0.0001, batch_siz
             print('[Train] Epoch [{}/{}], Step [{}/{}], Train-Loss: {:.4f}, Train-Acc: {:.2f} %'
                   .format(epoch + 1, epochs, i + 1, total_step, loss.item(), accuracy))
 
-            if i % total_step == 0:
-                train_acc.append(accuracy)
-                train_loss.append(loss)
+            train_acc_it.append(accuracy)
+            train_loss_it.append(loss.item())
+
+        train_acc.append(np.mean(np.array(train_acc_it)))
+        train_loss.append(np.mean(np.array(train_loss_it)))
 
         net.eval()
         with torch.no_grad():
@@ -76,24 +80,33 @@ def train_model(images, labels, path, epochs=10, learning_rate=0.0001, batch_siz
                 correct += (predicted == labels).sum().item()
                 accuracy = 100 * correct / total
 
-                if i % total_step == 0:
-                    test_acc.append(accuracy)
-                    test_loss.append(loss)
-                    roc_score.append(np.array(predicted).reshape(-1))
-                    roc_true.append(np.array(labels).reshape(-1))
+                roc_score_it.extend(np.array(outputs.data).reshape(-1))
+                roc_true_it.extend(np.array(labels).reshape(-1))
 
-                print('[Test] Epoch [{}/{}], Step [{}/{}], Test-Loss: {:.4f}, Test-Acc: {:.2f}'
+                test_acc_it.append(accuracy)
+                test_loss_it.append(loss.item())
+
+                print('[Test] Epoch [{}/{}], Step [{}/{}], Test-Loss: {:.4f}, Test-Acc: {:.2f}%'
                       .format(epoch + 1, epochs, i + 1, total_step, loss.item(), accuracy))
+
+            if (epoch + 1) % 10 == 0:
+                roc_score.append(roc_score_it)
+                roc_true.append(roc_true_it)
+
+            test_acc.append(np.mean(np.array(test_acc_it)))
+            test_loss.append(np.mean(np.array(test_loss_it)))
 
     # Save the model checkpoint
     torch.save(net.state_dict(), path)
 
     # ROC
-    true = np.array(roc_true).reshape(-1)
-    score = np.array(roc_score).reshape(-1)
-    fpr, tpr, thresholds = metrics.roc_curve(true, score)
+    if epochs > 9:
+        true = np.array(roc_true)
+        score = np.array(roc_score)
+        print(true.shape)
 
-    plot_roc_binary(fpr, tpr, './results/simple_classifier_roc.pdf')
+
+        plot_roc_binary(true, score, './results/simple_classifier_roc.pdf')
     plot_loss(train_loss, test_loss, './results/simple_classifier_loss.pdf')
     plot_acc(train_acc, test_acc, './results/simple_classifier_acc.pdf')
 
--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import metrics
 
 
-def plot_roc_binary(fpr, tpr, path):
-    plt.plot(fpr, tpr, label='RF')
+def plot_roc_binary(true, score, path):
+    for ix, (tr, sc) in enumerate(zip(true, score)):
+        fpr, tpr, thresholds = metrics.roc_curve(tr, sc)
+        plt.plot(fpr, tpr, label=f'Epoch {(ix + 1) * 10}')
     plt.xlabel('False positive rate')
     plt.ylabel('True positive rate')
     plt.title('ROC curve')
@@ -27,16 +30,18 @@ def plot_loss(train_loss, test_loss, path):
     plt.legend(frameon=False)
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
+    plt.title('ROC curve')
     plt.savefig(path)
     plt.show()
 
 
-def plot_acc(train_acc, test_acc, path):
+def plot_acc(train_acc, test_acc, path, name):
     plt.plot(train_acc, label='Training Acc')
     plt.plot(test_acc, label='Validation Acc')
     plt.legend(frameon=False)
     plt.ylabel('Percentage')
     plt.xlabel('Epoch')
+    plt.title(f'Binary accuracy of')
     plt.savefig(path)
     plt.show()
 
